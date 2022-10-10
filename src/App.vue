54b86dbb942713c8d0eb0ee2a31a777a487ea9b3
Dominik Fedora <dominik.a.hoffmann@fau.de>
2022-10-10 23:17:06 +0200

    Changed primary color to dark red + changed font

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
 </script>
 <style>
 *{
+  @import url('https://fonts.googleapis.com/css2?family=Inter+Tight:wght@300;500&display=swap');
+  font-family: 'Inter Tight', sans-serif;
+
  margin: 0;
  padding: 0; 
 }
