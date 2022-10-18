1ee7451ade3a462c91e5acc9eef27592accc5d3b
Dominik Fedora <dominik.a.hoffmann@fau.de>
2022-10-18 21:43:51 +0200

    Added page for exchange rate tool + link back to home when clicking title

---
+++ src/view/ExchangeRateTool.vue
@@ -0,0 +1,26 @@
+<template>
+   
+    <NavBarComponent title="Dominik Hoffmann"></NavBarComponent>
+    <h3>Exchange Rate Tool</h3>
+    
+    
+
+
+
+</template>
+<script>
+import NavBarComponent from '@/components/NavBarComponent.vue';
+
+export default {
+name: 'ExhangeRateTool',
+components: {  NavBarComponent },
+
+mounted () {
+    console.log('home init')
+}
+}
+
+</script>
+<style>
+
+</style>
