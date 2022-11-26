9f64101637103b4fefc50cd32fbcb26853b93d4e
Dominik Fedora Desktop <dominik151099@outlook.de>
2022-11-26 16:47:26 +0100

    Added missing fields for output, output selection and control buttons

--- src/view/ExchangeRateTool.vue
+++ src/view/ExchangeRateTool.vue
@@ -5,7 +5,7 @@
       <h2 id="ERT-Header">Currency Converter</h2>
       <div class="input-section">
         <div class="input-field">
-          <q-input rounded outlined type="number" v-model="amount" bg-color="white"/> 
+          <q-input rounded outlined type="number" v-model="amount" bg-color="white"/>
         </div>
         <div class="currency-selector">
           <q-select filled bg-color="white"
@@ -15,29 +15,70 @@
                     label="Choose Currency"
                     color="lightgrey"
                     options-selected-class="text-deep-orange"
-                   >
-             <template v-slot:option="scope" >
-                <q-item v-bind="scope.itemProps.valueOf()" >
+          >
+            <template v-slot:option="scope">
+              <q-item v-bind="scope.itemProps.valueOf()">
                 <q-item-section style=" margin: 0; padding: 10px">
                   <img :src="this.getFlagForCountry(scope.opt.country)"
-                         width="40"
-                         height="30"
-                         style="margin-right: auto; border: #1D1D1D solid 1px">
+                       width="40"
+                       height="30"
+                       style="margin-right: auto; border: #1D1D1D solid 1px">
                 </q-item-section>
                 <q-item-section>
 
-                  <q-item-label >{{ scope.opt.value }} <span style="font-weight: bold; font-size: 150%">{{scope.opt.sign}}</span></q-item-label>
+                  <q-item-label>{{ scope.opt.value }} <span
+                      style="font-weight: bold; font-size: 150%">{{ scope.opt.sign }}</span></q-item-label>
 
 
-
-                  
-              </q-item-section>
+                </q-item-section>
               </q-item>
-             </template> 
+            </template>
           </q-select>
         </div>
       </div>
+      <div class="center-buttons button-section">
+        <q-btn id="convert-button" push color="white" text-color="black" icon="autorenew" padding="lg"/>
+        <q-btn id="switch-button" push color="white" text-color="black" icon="swap_vert" padding="lg"/>
+
+      </div>
       <div class="output-section">
+        <div class="input-field">
+          <q-field filled rounded bg-color="white">
+            <template v-slot:control>
+              <div class="full-width "></div>
+            </template>
+
+          </q-field>
+        </div>
+        <div class="currency-selector">
+          <q-select filled bg-color="white"
+                    v-model="toCurrencySelection"
+                    :options="currencies"
+                    :display-value="getDisplayValueTo()"
+                    label="Choose Currency"
+                    color="lightgrey"
+                    options-selected-class="text-deep-orange"
+          >
+            <template v-slot:option="scope">
+              <q-item v-bind="scope.itemProps.valueOf()">
+                <q-item-section style=" margin: 0; padding: 10px">
+                  <img :src="this.getFlagForCountry(scope.opt.country)"
+                       width="40"
+                       height="30"
+                       style="margin-right: auto; border: #1D1D1D solid 1px">
+                </q-item-section>
+                <q-item-section>
+
+                  <q-item-label>{{ scope.opt.value }} <span
+                      style="font-weight: bold; font-size: 150%">{{ scope.opt.sign }}</span></q-item-label>
+
+
+                </q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+        </div>
+
       </div>
     </div>
   </div>
@@ -45,55 +86,75 @@
 <script>
 import ExchangeRateModel from '../classes/exchangeRateModel'
 import NavBarComponent from "@/components/NavBarComponent.vue";
-import { ref } from 'vue'
+import {ref} from 'vue'
+
 export default {
   name: "ExhangeRateTool",
-  components: { NavBarComponent },
+  components: {NavBarComponent},
   setup() {
-      return{
-        fromCurrencySelection: ref(null),
-        amount: 0,
+    return {
+      fromCurrencySelection: ref(null),
+      toCurrencySelection: ref(null),
+      amount: 0,
 
-        currencies: [
-          {
-            value: "United States Dollar",
-            country: "us",
-            currency: "USD",
-            sign: "$"
+      currencies: [
+        {
+          value: "United States Dollar",
+          country: "us",
+          currency: "USD",
+          sign: "$"
 
-          },
-          {
-            value: "Euro",
-            country: "eu",
-            currency: "EUR",
-            sign: "€"
-          },
-          {
-            value: "Russian Rubles",
-            country: "ru",
-            currency: "RUB",
-            sign: "₽"
-          },
-          {
-            value: "Chinese Renminbi",
-            country: "cn",
-            currency: "CNY",
-            sign: "¥"
-          },
-          {
-            value: "Japanese Yen",
-            country: "jp",
-            currency: "JPY",
-            sign: "¥"
-          }
+        },
+        {
+          value: "Euro",
+          country: "eu",
+          currency: "EUR",
+          sign: "€"
+        },
+        {
+          value: "Russian Rubles",
+          country: "ru",
+          currency: "RUB",
+          sign: "₽"
+        },
+        {
+          value: "Chinese Yuan Renminbi",
+          country: "cn",
+          currency: "CNY",
+          sign: "¥"
+        },
+        {
+          value: "Japanese Yen",
+          country: "jp",
+          currency: "JPY",
+          sign: "¥"
+        },
+        {
+          value: "Polish Złoty",
+          country: "pl",
+          currency: "PLN",
+          sign: "zł"
+        },
+        {
+          value: "Hungarian Forint",
+          country: "hu",
+          currency: "HUF",
+          sign: "Ft"
+        },
+        {
+          value: "British Pound Sterling",
+          country: "gb",
+          currency: "GBP",
+          sign: "£"
+        }
 
-        ]
-      }
+      ]
+    }
   },
   data() {
     return {
-        exchangeRateModel : new ExchangeRateModel,
-        unused: 0
+      exchangeRateModel: new ExchangeRateModel,
+      unused: 0
     }
   },
   async mounted() {
@@ -104,76 +165,114 @@ export default {
     console.log("home init");
   },
   methods: {
-    getFlagForCountry(countryID){
-      return "https://flagcdn.com/w40/"+ countryID  +".png";
+    getFlagForCountry(countryID) {
+      return "https://flagcdn.com/w40/" + countryID + ".png";
     },
-    getDisplayValue(){
-      if(this.fromCurrencySelection == null){
+    getDisplayValue() {
+      if (this.fromCurrencySelection == null) {
         return "Select Currency";
-      }
-      else{
+      } else {
         return this.fromCurrencySelection.value + " " + this.fromCurrencySelection.sign
+      }
+    },
+    getDisplayValueTo() {
+      if (this.toCurrencySelection == null) {
+        return "Select Currency";
+      } else {
+        return this.toCurrencySelection.value + " " + this.toCurrencySelection.sign
       }
     }
   },
   watch: {
-    fromCurrencySelection(selection){
-      if(selection != null){
+    fromCurrencySelection(selection) {
+      if (selection != null) {
         console.log(selection.value)
 
       }
     }
   }
- 
+
 };
 </script>
 <style>
 #ERT-Header {
-    text-align: center;
-    width: 40rem;
-    background-color: #049a30;
-    font-weight: 700;
-    margin-left: auto; margin-right: auto;
-    -webkit-text-stroke: 1.5px  white;
-    border-radius: 5%;
-    margin-top: 1%;
-    
+  text-align: center;
+  width: 40rem;
+  background-color: #049a30;
+  font-weight: 700;
+  margin-left: auto;
+  margin-right: auto;
+  -webkit-text-stroke: 1.5px white;
+  border-radius: 5%;
+  margin-top: 1%;
+
 
 }
+
 .container {
   margin-top: 5%;
   display: flex;
   justify-content: center;
 }
+
 .Toolbox {
   display: flex;
-  
+
   flex-direction: column;
-  
+
   height: 50rem;
   width: 100rem;
   background-image: url("https://www.publicdomainpictures.net/pictures/400000/nahled/image-16183020011ky.png");
   border-radius: 2%;
 }
+
 .input-section {
   display: flex;
-    justify-content: space-evenly;
-    padding-top: 2%;
-    
-    
-    
-    margin-left: auto; margin-right: auto;
+  justify-content: space-evenly;
+  padding-top: 2%;
+
+
+  margin-left: auto;
+  margin-right: auto;
 }
-.currency-selector{
+
+.currency-selector {
   margin-left: 10px;
   width: 25rem;
 }
-.input-field{
+
+.input-field {
   width: 20rem;
 }
 
+.output-section {
+  display: flex;
+  justify-content: space-evenly;
+  padding-top: 4%;
 
-html{
+
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.button-section {
+  display: flex;
+  align-content: space-between;
+  padding-top: 4%;
+  margin-left: 30%;
+  margin-right: 30%;
+}
+
+#switch-button {
+  margin-left: 20%;
+}
+
+#convert-button {
+  margin-right: 20%;
+}
+
+
+html {
   background-color: gainsboro;
 }
 </style>
